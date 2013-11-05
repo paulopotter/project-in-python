8885961da3c29794a2c27fc8e6f92de29045fc07
Paulo Oliveira <paulofrauches@gmail.com>
2013-11-05 10:53:44 -0200

    Pagina 32

--- TDD-Book/test_tddBook.py
+++ TDD-Book/test_tddBook.py
@@ -1,7 +1,7 @@
 import unittest
 import tddBookMoney 
 from tddBookDollar import Dollar
-import tddBookFranc 
+from tddBookFranc import Franc
 
 class testTddBook(unittest.TestCase):
 	def testMultiplication(self):
@@ -10,9 +10,16 @@ class testTddBook(unittest.TestCase):
 		dollarFive = Dollar(5).times(3)
 		self.assertEquals(15,dollarFive)
 
+		francFive = Franc(5).times(2)
+		self.assertEquals(10,francFive)
+		francFive = Franc(5).times(3)
+		self.assertEquals(15,francFive)
+
 	def testEquallity(self):
 		self.assertTrue(Dollar(5).equals(Dollar(5).amount))
 		self.assertFalse(Dollar(5).equals(Dollar(6).amount))
+		self.assertTrue(Franc(5).equals(Franc(5).amount))
+		self.assertFalse(Franc(5).equals(Franc(6).amount))
 
 if __name__ == '__main__':
 	unittest.main()
