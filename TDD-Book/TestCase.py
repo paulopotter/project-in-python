2074fe9b3cc3b2d1cf3f67b2073f4c697e7fbf21
Paulo Oliveira <paulofrauches@gmail.com>
2013-11-07 15:26:06 -0200

    Pagina  81

--- TDD-Book/TestCase.py
+++ TDD-Book/TestCase.py
@@ -1,3 +1,8 @@
 #Perguntar sobre teste que depende de outro, por exemplo, se o que eu for fazer necessita de algo que o outro teste passou, o que fazer...
 # parei na pagina 81
 
+class TestCase:
+	def __init__(self,name):
+		self.name = name
+	def run(self):
+		exec "self." + self.name + "()"
--- TDD-Book/testPython_tddBook.py
+++ TDD-Book/testPython_tddBook.py
@@ -1,7 +1,12 @@
 from WasRun import WasRun
+import TestCase
+class TestCaseTest(TestCase):
+	test = WasRun("testMethod")
+	assert(not test.wasRun)
+	test.run()
+	assert(test.wasRun)
 
-
-test = WasRun("testMethod")
-print test.wasRun
-test.run()
-print test.wasRun
+TestCaseTest("testRunning").run()
+# print test.wasRun
+# test.run()
+# print test.wasRun
