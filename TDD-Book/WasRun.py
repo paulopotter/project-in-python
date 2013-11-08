ec52704464cfbc0f5ba71a1e0eb9a69020a2aa95
Paulo Oliveira <paulofrauches@gmail.com>
2013-11-08 10:15:13 -0200

    Pagina  84

--- TDD-Book/WasRun.py
+++ TDD-Book/WasRun.py
@@ -4,7 +4,15 @@ class WasRun(TestCase):
 	def __init__(self,name):
 		self.wasRun = None
 		TestCase.__init__(self,name)
+
+	def setUp(self):
+		self.log="setUp"
+
 	def testMethod(self):
-		self.wasRun = 1
+		self.log = self.log + "testMethod"
+
+	def tearDown(self):
+		self.log = self.log + "tearDown"
+
 	def run(self):
 		exec "self." + self.name +"()"
--- TDD-Book/testPython_tddBook.py
+++ TDD-Book/testPython_tddBook.py
@@ -1,12 +1,31 @@
 from WasRun import WasRun
-import TestCase
-class TestCaseTest(TestCase):
-	test = WasRun("testMethod")
-	assert(not test.wasRun)
-	test.run()
-	assert(test.wasRun)
+from TestCase import TestCase
 
-TestCaseTest("testRunning").run()
+class TestResult:
+	def __init__(self):
+		self.runCount = 0
+	def testStarted(self):
+		self.runCount = self.runCount + 1
+	def summary(self):
+		return "%d  run, 0 failed" % self.runCount
+
+
+
+
+class TestCaseTest(TestCase):
+	def setUp(self):
+		self.test = WasRun("testMethod")
+	def testTemplateMethod(self):
+		test = WasRun("testMethod")
+		test.run()
+		assert("setUp testMethod tearDown" == self.test.log)
+
+	def testResult(self):
+		test = WasRun("testMethod")
+		result = test.run
+		assert("1 run, 0 failed" == result.summary())
+
+TestCaseTest("testSetUp").run()
 # print test.wasRun
 # test.run()
 # print test.wasRun
