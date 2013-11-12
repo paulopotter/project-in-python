bf5eabab983b8247d88447cfbdeade52786add87
Paulo Oliveira <paulofrauches@gmail.com>
2013-11-12 09:43:37 -0200

    Criada a estrutura base

--- Bodgitt-And-Scarper/dbSchema.py
+++ Bodgitt-And-Scarper/dbSchema.py
@@ -44,46 +44,52 @@ for x in range(0, numNumberOfFields):
 	# print "end of Repeating Block:",numEndOfRepeatingBlock
 	# print '-'*20
 	tupla.append(strFieldName)
+	# tupla.append(numEndOfRepeatingBlock)
 
-print tupla
+print "MetaDados :",tupla
 print '-'*21
-
 tupla2 =[]
+dicionario = {}
+estrutura1 = []
+estrutura2 = []
+estrutura3 = []
+estrutura4 = []
+estrutura5 = []
+estrutura6 = []
 # Dados
 for y in range(0,3):
 # for y in range(0,numTotalOverallLength - numNumberOfFields):
 	
-	# #Bytes of Field Name
-	bytesOfFieldName = struct.unpack('B 30s B B 62s B B 62s B B 4s B B 6s 2B B 6s B',ReadBytes(fd,1+30+1+1+62+1+1+62+1+1+4+1+1+6+1+1+6+2))
-	# numBytesOfFieldName = int(bytesOfFieldName[1])
+	#Bytes of Field Name
+	bytesOfFieldName = struct.unpack('B 30s 2B 62s 2B 62s 2B 4s 2B 6s 2B 6s B',ReadBytes(fd,numTotalOverallLength))
+	byteFlag = struct.unpack('B',ReadBytes(fd,1))
 
-	#Nome do Campo
-	# fieldName = struct.unpack('30s',ReadBytes(fd,30))
-	# strFieldName = fieldName[0]
-
-	# # End of Repeating Block
-	# endOfRepeatingBlock = struct.unpack('B',ReadBytes(fd,1))
-	# numEndOfRepeatingBlock = int(endOfRepeatingBlock[1])
-
-
-	# print totalOverallLength
-	print "Field Length: ",bytesOfFieldName
-	# print "Field Length: ",numBytesOfFieldName
-	# print "Field Name:",fieldName
-	# # print "Field Name:",strFieldName
-	# print "end of Repeating Block:",endOfRepeatingBlock
-	# print "end of Repeating Block:",numEndOfRepeatingBlock
-	print '-'*20
-	# tupla2.append(strFieldName)
+	# print "Field Length: ",bytesOfFieldName
+	# print '-'*20
 	tupla2.append(bytesOfFieldName)
+	estrutura1.append(tupla2[y][1].strip())
+	estrutura2.append(tupla2[y][4].strip())
+	estrutura3.append(tupla2[y][7].strip())
+	estrutura4.append(tupla2[y][10].strip())
+	estrutura5.append(tupla2[y][13].strip())
+	estrutura6.append(tupla2[y][16].strip())
 
-# print tupla2
+# print tupla2 
+dicionario[tupla[0]] = estrutura1
+dicionario[tupla[1]] = estrutura2
+dicionario[tupla[2]] = estrutura3
+dicionario[tupla[3]] = estrutura4
+dicionario[tupla[4]] = estrutura5
+dicionario[tupla[5]] = estrutura6
 
 
 print '\n'
-# print "magic Cookie Value : ",magicCookieValue
-# print "num Total Overall Length :",numTotalOverallLength
-# print "num Number Of Fields :",numNumberOfFields
+print "Data: ",dicionario
+
+print '\n'
+print "magic Cookie Value : ",magicCookieValue
+print "num Total Overall Length :",numTotalOverallLength
+print "num Number Of Fields :",numNumberOfFields
 
 
 
