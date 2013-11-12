4536bc8f1fdf542ca78cf1a560689bd0408f7c70
Paulo Oliveira <paulofrauches@gmail.com>
2013-11-12 15:07:17 -0200

    Estrutura final Realizada

--- Bodgitt-And-Scarper/dbSchema.py
+++ Bodgitt-And-Scarper/dbSchema.py
@@ -1,38 +1,36 @@
 import struct #pack e unpack
 import pdb #Debug
 import sys #exit()
-
+arquivo = './db-2x1.db'
 tuplaMetaDados = []
+tuplaMetaFieldLength = []
 tuplaDados =[]
 dicionario = {}
-fd = open('./db-2x1.db','rb+')
+tuplaByteFlag = []
+listDados = []
 
-def tamanhoArquivo(fd1 ='./db-2x1.db'):
+fd = open(arquivo,'rb+')
+
+def tamanhoArquivo(fd1 = arquivo):
 	fda = open(fd1,'rb')
 	tamanho = len(fda.read())
 	fda.close()
 	return tamanho
 
-
 def ReadBytes(fd, noBytes):
- data = fd.read(noBytes)
- return data
-
-
-
+	data = fd.read(noBytes)
+	return data
 
 # Magic Cookie
 magicCookieValue = struct.unpack('4B',ReadBytes(fd,4))
 
-#Total Overall Length
+# Total Overall Length
 totalOverallLength = struct.unpack('4B',ReadBytes(fd,4))
 numTotalOverallLength = int(totalOverallLength[3])
 
 # Number of Fields
 numberOfFields = struct.unpack('2B',ReadBytes(fd,2))
 numNumberOfFields = int(numberOfFields[1])
-
-
 
 # MetaDado
 for x in range(numNumberOfFields):
@@ -49,38 +47,34 @@ for x in range(numNumberOfFields):
 	endOfRepeatingBlock = struct.unpack('2B',ReadBytes(fd,2))
 	numEndOfRepeatingBlock = int(endOfRepeatingBlock[1])
 
-	# print totalOverallLength
-	# print "Field Length: ",numBytesOfFieldName
-	# print "Field Name:",strFieldName
-	# print "end of Repeating Block:",numEndOfRepeatingBlock
-	# print '-'*20
 	tuplaMetaDados.append(strFieldName)
-	# tuplaMetaDados.append(numEndOfRepeatingBlock)
+	tuplaMetaFieldLength.append(numEndOfRepeatingBlock)
 
 
 # Cria as variaveis metaDado(x) dinamicamente
 for i in range(numNumberOfFields):
 	locals()['metaDado%d'%i] = []
-tuplaByteFlag = []
 
-print numberOfFields
 # Dados
+# for y in range(3):
 for y in range(int(tamanhoArquivo())/int(numTotalOverallLength)):
 	# Byte flag
 	byteFlag = struct.unpack('B',ReadBytes(fd,1))
 	#Bytes of Field Name
-	bytesOfFieldName = struct.unpack('30s 2B 62s 2B 62s 2B 4s 2B 6s 2B 6s 2B',ReadBytes(fd,numTotalOverallLength))
+	bytesOfFieldName = struct.unpack(str(tuplaMetaFieldLength[0]-2)+'s 2B '+str(tuplaMetaFieldLength[1]-2)+'s 2B '+str(tuplaMetaFieldLength[2]-2)+'s 2B '+str(tuplaMetaFieldLength[3]-2)+'s 2B '+str(tuplaMetaFieldLength[4]-2)+'s 2B '+str(tuplaMetaFieldLength[5]-2)+'s 2B',ReadBytes(fd,numTotalOverallLength))
 
 	tuplaDados.append(bytesOfFieldName)
-	metaDado0.append(tuplaDados[y][0].strip())
-	metaDado1.append(tuplaDados[y][3].strip())
-	metaDado2.append(tuplaDados[y][6].strip())
-	metaDado3.append(tuplaDados[y][9].strip())
-	metaDado4.append(tuplaDados[y][12].strip())
-	metaDado5.append(tuplaDados[y][15].strip())
-	tuplaByteFlag.append(byteFlag[0])
+	listDados.append({
+					tuplaMetaDados[0]:tuplaDados[y][0].strip(),
+					tuplaMetaDados[1]:tuplaDados[y][3].strip(),
+					tuplaMetaDados[2]:tuplaDados[y][6].strip(),
+					tuplaMetaDados[3]:tuplaDados[y][9].strip(),
+					tuplaMetaDados[4]:tuplaDados[y][12].strip(),
+					tuplaMetaDados[5]:tuplaDados[y][15].strip(),
+				   'Byte Flag':      byteFlag[0]
+				   })
 
-
+#fechando o arquivo aberto
 fd.close()
 
 # Adicionando todos os campos metadado(x) no dicionario
@@ -89,18 +83,14 @@ for i in range(numNumberOfFields):
 dicionario["byteFlag"] = tuplaByteFlag
 
 
-
-
-#-------------
-
-print "MetaDados :",tuplaMetaDados
-print '-'*21
-print '\n'
-print "Data: ",dicionario
-print '\n'
-print "magic Cookie Value : ",magicCookieValue
-print "num Total Overall Length :",numTotalOverallLength
-print "num Number Of Fields :",numNumberOfFields
-
-
-
+#------- Exibindo ------
+print '='*50,'\n'
+print listDados[0]
+print '\n','-'*21,'\n'
+print listDados[1]
+print '\n','-'*21,'\n'
+print listDados
+# print "MetaDados :",tuplaMetaDados
+# print "Data: ",dicionario
+# print '\n','-'*21,'\n'
+print '\n','='*50
