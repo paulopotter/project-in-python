d6d09acea9ba480d957aced0534898502e725cce
Paulo Oliveira <paulofrauches@gmail.com>
2013-11-13 08:21:51 -0200

    Primeiro Teste

--- Bodgitt-And-Scarper/rotinaDeTestes.py
+++ Bodgitt-And-Scarper/rotinaDeTestes.py
@@ -2,10 +2,10 @@ import unittest
 import dbSchema
 
 class TestFile(unittest.TestCase):
-	def testMagicCoookie(self):
-		self.assertIsInstance(dbSchema.magicCookieValue,tuple)
 
-
+	def test_totalLarguraLinha(self):
+		# total overall length in bytes of each record
+		self.assertEqual(dbSchema.numTotalOverallLength,sum(dbSchema.tuplaMetaFieldLength))
 
 if __name__ == '__main__':
 	unittest.main()
