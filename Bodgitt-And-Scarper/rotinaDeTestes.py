9bb824b2c0e7c35d2334892eae1cad894c9abb46
Paulo Oliveira <paulofrauches@gmail.com>
2013-11-14 09:33:56 -0200

    atualizaçao de logica

--- Bodgitt-And-Scarper/rotinaDeTestes.py
+++ Bodgitt-And-Scarper/rotinaDeTestes.py
@@ -3,19 +3,16 @@ import dbSchema
 
 class TestDBSchema(unittest.TestCase):
 
-	# def test_IndentifiesDataFile(self):
-	# 	self.assertEqual(dbSchema.magicCookieValue, (0,0,2,1))
-
 	def test_total_overall_length(self):
-		# total overall length in bytes of each record 
+		# total overall length in bytes of each record
 		self.assertEqual(dbSchema.number_total_overall_length, sum(dbSchema.tuple_meta_field_length))
 
 	def test_length_for_fields_name(self):
 		# number of fields in each record
-		j = -3
+		j = 0
 		for x in range(len(dbSchema.tuple_meta_field_length)):
-			j += 3
-			self.assertEqual(len(dbSchema.bytes_of_field_name[j]) + 2, dbSchema.tuple_meta_field_length[x])
+			self.assertEqual(len(dbSchema.bytes_of_field_name[j]), dbSchema.tuple_meta_field_length[x])
+			j += 1
 
 
 if __name__ == '__main__':
