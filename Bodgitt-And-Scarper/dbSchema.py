8707477965d8ff71c354ffe40e83c57f09fde179
Paulo Oliveira <paulofrauches@gmail.com>
2013-11-18 14:55:47 -0200

    Alterado as estrutura por causa do teste (setUp)

--- Bodgitt-And-Scarper/dbSchema.py
+++ Bodgitt-And-Scarper/dbSchema.py
@@ -1,27 +1,25 @@
 import struct
-class dbSchema:
+
+
+class DbSchema:
     def __init__(self):
         self.chosen_file = './db-2x1.db'
-        self.open_file   = open(self.chosen_file, 'rb')
+        self.open_file = open(self.chosen_file, 'rb')
 
     def length_of_file(self):
-        size_of_file = len(self.open_file.read())
-        return size_of_file
+        return len(self.open_file.read())
 
     def read_bytes(self,open_file,number_of_bytes):
-        data = open_file.read(number_of_bytes)
-        return data
+        return open_file.read(number_of_bytes)
 
     def unpack_file(self,number_of_bytes,format_character,open_file):
-        return struct.unpack(str(number_of_bytes) + format_character, dbSchema().read_bytes(open_file,number_of_bytes))
+        return struct.unpack(str(number_of_bytes) + format_character, self.read_bytes(open_file,number_of_bytes))
 
     def start_of_file(self):
-        open_file = dbSchema().open_file
+        magic_cookie = self.unpack_file(4,"B", self.open_file)
 
-        magic_cookie = dbSchema().unpack_file(4,"B",open_file)
+        total_overall_length = self.unpack_file(4,"B", self.open_file)
 
-        total_overall_length = dbSchema().unpack_file(4,"B",open_file)
+        number_of_fields = self.unpack_file(2, "B", self.open_file)
 
-        number_of_fields = dbSchema().unpack_file(2, "B", open_file)
-
-        return {"magic_cookie":magic_cookie[-1], "total_overall_length":total_overall_length[-1], "number_of_fields":number_of_fields[-1] }
+        return {"magic_cookie": magic_cookie[-1], "total_overall_length": total_overall_length[-1], "number_of_fields": number_of_fields[-1]}
