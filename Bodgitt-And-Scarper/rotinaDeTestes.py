d33615b9d277579553bf51aae05ff30845e281da
Paulo Oliveira <paulofrauches@gmail.com>
2013-11-18 08:39:26 -0200

    Criado o primeiro teste de length_of_file

--- Bodgitt-And-Scarper/rotinaDeTestes.py
+++ Bodgitt-And-Scarper/rotinaDeTestes.py
@@ -1,18 +1,14 @@
 import unittest
 import dbSchema
 
+db_schema = dbSchema.dbSchema()
+
 class TestDBSchema(unittest.TestCase):
 
-	def test_total_overall_length(self):
-		# total overall length in bytes of each record
-		self.assertEqual(dbSchema.number_total_overall_length, sum(dbSchema.tuple_meta_field_length))
+    def test_file_not_empty(self):
+        self.assertNotEqual(db_schema.length_of_file(),0)
 
-	def test_length_for_fields_name(self):
-		# number of fields in each record
-		j = 0
-		for x in range(len(dbSchema.tuple_meta_field_length)):
-			self.assertEqual(len(dbSchema.bytes_of_field_name[j]), dbSchema.tuple_meta_field_length[x])
-			j += 1
+
 
 
 if __name__ == '__main__':
