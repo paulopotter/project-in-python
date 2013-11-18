ff4586c5934ac18a82ebee70c96446f5e5994e8b
Paulo Oliveira <paulofrauches@gmail.com>
2013-11-18 14:55:15 -0200

    adicionado o setUp

--- Bodgitt-And-Scarper/rotinaDeTestes.py
+++ Bodgitt-And-Scarper/rotinaDeTestes.py
@@ -1,15 +1,21 @@
 import unittest
 import dbSchema
 
-db_schema = dbSchema.dbSchema()
 
 class TestDBSchema(unittest.TestCase):
 
+    def setUp(self):
+        self.db_schema = dbSchema.DbSchema()
+
     def test_file_not_empty(self):
-        self.assertNotEqual(db_schema.length_of_file(),0)
+        self.assertNotEqual(self.db_schema.length_of_file(),0)
 
     def test_start_of_file(self):
-        self.assertIsInstance(db_schema.start_of_file(),dict)
+        self.assertIsInstance(self.db_schema.start_of_file(),dict)
+
+    # def test_schema_description(self):
+    #     self.assertIsInstance(db_schema.schema_description(),dict)
+    # print db_schema.schema_description()
 
 
 
