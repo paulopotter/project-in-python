18fd4217022e21ed749502d38cc545c7d0749823
Paulo Oliveira <paulofrauches@gmail.com>
2013-11-19 08:42:26 -0200

    Criada a funçao schema_description que retorna uma lista do nome dos campos e os tamanhos permitido em cada campo da tabela

--- Bodgitt-And-Scarper/dbSchema.py
+++ Bodgitt-And-Scarper/dbSchema.py
@@ -4,7 +4,7 @@ import struct
 class DbSchema:
     def __init__(self):
         self.chosen_file = './db-2x1.db'
-        self.open_file = open(self.chosen_file, 'rb')
+        self.open_file = open(self.chosen_file, 'rb+')
 
     def length_of_file(self):
         return len(self.open_file.read())
@@ -13,13 +13,36 @@ class DbSchema:
         return open_file.read(number_of_bytes)
 
     def unpack_file(self,number_of_bytes,format_character,open_file):
-        return struct.unpack(str(number_of_bytes) + format_character, self.read_bytes(open_file,number_of_bytes))
+        return struct.unpack(str(number_of_bytes) + format_character, self.read_bytes(open_file, number_of_bytes))
 
     def start_of_file(self):
-        magic_cookie = self.unpack_file(4,"B", self.open_file)
+        self.open_file.seek(0)
 
-        total_overall_length = self.unpack_file(4,"B", self.open_file)
+        magic_cookie = self.unpack_file(4, "B", self.open_file)
+
+        total_overall_length = self.unpack_file(4, "B", self.open_file)
 
         number_of_fields = self.unpack_file(2, "B", self.open_file)
 
-        return {"magic_cookie": magic_cookie[-1], "total_overall_length": total_overall_length[-1], "number_of_fields": number_of_fields[-1]}
+        return {"magic_cookie": magic_cookie, "total_overall_length": total_overall_length[-1], "number_of_fields": number_of_fields[-1]}
+
+
+    def schema_description(self):
+        start_of_file = self.start_of_file()
+        open_file = self.open_file
+        meta_dados = []
+        for x in range(start_of_file["number_of_fields"]):
+
+            bytes_of_field_name = self.unpack_file(2, "B", open_file)
+            bytes_of_field_name = bytes_of_field_name[-1]
+
+            field_name = self.unpack_file(bytes_of_field_name, 's', open_file)
+            field_name = field_name[0]
+
+            end_of_repeating_block = self.unpack_file(2, 'B', open_file)
+            end_of_repeating_block = end_of_repeating_block[-1]
+
+            meta_dados.append((field_name, end_of_repeating_block))
+
+        return meta_dados
+
