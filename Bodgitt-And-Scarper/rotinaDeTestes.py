47b02590cd3343972bde5714b9568cd2c5fd92e1
Paulo Oliveira <paulofrauches@gmail.com>
2013-11-19 12:59:49 -0200

    Adicionado o test_of_records

--- Bodgitt-And-Scarper/rotinaDeTestes.py
+++ Bodgitt-And-Scarper/rotinaDeTestes.py
@@ -8,15 +8,27 @@ class TestDBSchema(unittest.TestCase):
         self.db_schema = dbSchema.DbSchema()
 
     def test_file_not_empty(self):
-        self.assertNotEqual(self.db_schema.length_of_file(),0)
+        self.assertNotEqual(self.db_schema.length_of_file(), 0)
+
+    def test_number_of_lines(self):
+        self.assertNotEqual(self.db_schema.number_of_lines(), 0)
 
     def test_start_of_file(self):
-        self.assertIsInstance(self.db_schema.start_of_file(),dict)
+        self.assertIsInstance(self.db_schema.start_of_file(), dict)
 
     def test_schema_description(self):
         self.db_schema.start_of_file()
 
-        self.assertIsInstance(self.db_schema.schema_description(),list)
+        self.assertIsInstance(self.db_schema.schema_description(), list)
+
+    def test_number_of_records(self):
+        self.db_schema.start_of_file()
+        self.db_schema.schema_description()
+        records = self.db_schema.records()
+
+        self.assertEqual(len(records), self.db_schema.number_of_lines())
+
+
 
 
 if __name__ == '__main__':
