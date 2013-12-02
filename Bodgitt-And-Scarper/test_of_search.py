afcb025efa9ef2275995ff2adb2d31ba0370d152
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2013-12-02 11:55:48 -0200

    Alterado os parametros dos testes para verificar com letra minuscula ou parte do texto

--- Bodgitt-And-Scarper/test_of_search.py
+++ Bodgitt-And-Scarper/test_of_search.py
@@ -6,11 +6,11 @@ class TestSearch(unittest.TestCase):
         self.search_class = search.Search()
 
     def test_name_of_seach(self):
-        name = self.search_class.search_for_name('B')
-        self.assertIn('Buonarotti & Company', name[0].get('name'))
+        name = self.search_class.search_for_name('b')
+        self.assertIn('Buon', name[0].get('name'))
 
     def test_location_of_seach(self):
-        location = self.search_class.search_for_location('S')
+        location = self.search_class.search_for_location('s')
         self.assertIn('Smallville', location[0].get('location'))
 
 
