9ee5398594e2a0aefaa6c8ec5d90021c349a3a68
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2013-12-02 14:13:41 -0200

    Arrumado erro de ortografia

--- Bodgitt-And-Scarper/test_of_search.py
+++ Bodgitt-And-Scarper/test_of_search.py
@@ -5,11 +5,11 @@ class TestSearch(unittest.TestCase):
     def setUp(self):
         self.search_class = search.Search()
 
-    def test_name_of_seach(self):
+    def test_name_of_search(self):
         name = self.search_class.search_for_name('b')
         self.assertIn('Buon', name[0].get('name'))
 
-    def test_location_of_seach(self):
+    def test_location_of_search(self):
         location = self.search_class.search_for_location('s')
         self.assertIn('Smallville', location[0].get('location'))
 
