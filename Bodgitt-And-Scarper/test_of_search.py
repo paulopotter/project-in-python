0ba597a68a944058631cbcea2e1d9f1d833c1482
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2013-12-02 10:31:37 -0200

    Retirado espaço de linha desnecessario

--- Bodgitt-And-Scarper/test_of_search.py
+++ Bodgitt-And-Scarper/test_of_search.py
@@ -7,8 +7,7 @@ class TestSearch(unittest.TestCase):
 
     def test_name_of_seach(self):
         name = self.search_class.search_for_name('B')
-
-        self.assertIn('Buonarotti & Company',name[0].get('name'))
+        self.assertIn('Buonarotti & Company', name[0].get('name'))
 
     def test_location_of_seach(self):
         location = self.search_class.search_for_location('S')
