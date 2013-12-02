4e165a9521aa2de8a61161d624b3852b1773be4f
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2013-12-02 09:56:05 -0200

    Alterado as funçoes de busca de local e nome para retornar uma lista de dicionarios com todos os valores da linha encontrada. Adicionado a opçao de exibir tudo caso nenhum parametro seja passado para a busca. Adicionado a funçao para exibir os valores em formato de tabela no terminal

--- Bodgitt-And-Scarper/search.py
+++ Bodgitt-And-Scarper/search.py
@@ -7,25 +7,40 @@ class Search(object):
     def __init__(self):
         self.db_schema = dbSchema.DbSchema()
 
-    def search_for_name(self, value):
+    def search_for_name(self, value = ''):
         match_values = []
         formatted_records = self.db_schema.formatted_records()
         for number_of_lines in range(self.db_schema.number_of_records()):
-            if value in formatted_records[number_of_lines]['name']:
-                match_values.append(formatted_records[number_of_lines]['name'].strip())
+            if value != '*' and value in formatted_records[number_of_lines]['name']:
+                match_values.append(formatted_records[number_of_lines])
+            elif value == '*':
+                match_values.append(formatted_records[number_of_lines])
 
         return match_values
 
-    def search_for_location(self, value):
+    def search_for_location(self, value = ''):
         match_values = []
         formatted_records = self.db_schema.formatted_records()
         for number_of_lines in range(self.db_schema.number_of_records()):
-            if value in formatted_records[number_of_lines]['location']:
-                match_values.append(formatted_records[number_of_lines]['location'].strip())
+            if value != '*' and value in formatted_records[number_of_lines]['location']:
+                match_values.append(formatted_records[number_of_lines])
+            elif value == '*':
+                match_values.append(formatted_records[number_of_lines])
 
         return match_values
 
+    def search_in_table(self, dictionary):
+        table_column_widths = "| {0:32} | {1:64} | {2:64} | {3:6} | {4:8} | {5:8} |"
+        table_header = table_column_widths.format("Name", "Location", "specialties", "size", "rate", "owner") + "\n+" + "="*180 + "+"
+        table_data = ""
 
+        for values in dictionary:
+            table_data += table_column_widths.format(values["name"], values["location"], values["specialties"], values["size"], values["rate"], values["owner"]) + "\n+" + "-"*180 + "+"
+
+        if len(dictionary) < 0:
+            return table_header + '\n'+ table_data
+        else:
+            return 'ERROR'
 
 
 
