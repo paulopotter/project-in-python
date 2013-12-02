09148b1740b1c7191b28b0cdf57bf0d97b092b94
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2013-12-02 14:12:33 -0200

    Alterado o valor padrao dos parametros de chamada de arquivo e criado as verificaçoes de exibiçao (o que exibir quando os parametros de chamada for utilizado)

--- Bodgitt-And-Scarper/show_data.py
+++ Bodgitt-And-Scarper/show_data.py
@@ -10,10 +10,22 @@ search_class = search.Search()
 def searching():
     parser = argparse.ArgumentParser(description = 'DESCRIPTION: Arquivo de busca para o dbSchema')
 
-    parser.add_argument('--name, --n', action = 'store', dest = 'name', default = '*', required = False, help = 'Name of data')
-    parser.add_argument('--location, --loc', action = 'store', dest = 'location', default = '*', required = False, help = 'Location of data')
+    parser.add_argument('--name, --n', action = 'store', dest = 'name', default = '', required = False, help = 'Name of data')
+    parser.add_argument('--location, --loc', action = 'store', dest = 'location', default = '', required = False, help = 'Location of data')
 
     arguments = parser.parse_args()
     return {'name' : arguments.name,'location' : arguments.location}
 
-print search_class.search_in_table(search_class.search_for_name(searching()['name']))
+
+if searching()['name'] != '' :
+    if searching()['location'] == '':
+        print search_class.formatted_table(search_class.search_for_name(searching()['name']), {})
+    else:
+        print search_class.formatted_table(search_class.search_for_name(searching()['name']), search_class.search_for_location(searching()['location'])
+            )
+else:
+    if searching()['location'] != '':
+        print search_class.formatted_table({}, search_class.search_for_location(searching()['location']))
+
+    else:
+        print search_class.formatted_table(search_class.search_for_name('*'), search_class.search_for_location('*'))
