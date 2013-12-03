b1f2f8cfcbee604041b84fa753c5f9a0662a902c
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2013-12-03 08:29:06 -0200

    Alterada a chamada de parametro

--- Bodgitt-And-Scarper/show_data.py
+++ Bodgitt-And-Scarper/show_data.py
@@ -10,8 +10,8 @@ search_class = search.Search()
 def searching():
     parser = argparse.ArgumentParser(description = 'DESCRIPTION: Arquivo de busca para o dbSchema')
 
-    parser.add_argument('--name, --n', action = 'store', dest = 'name', default = '', required = False, help = 'Name of data')
-    parser.add_argument('--location, --loc', action = 'store', dest = 'location', default = '', required = False, help = 'Location of data')
+    parser.add_argument('--name, -n', action = 'store', dest = 'name', default = '', required = False, help = 'Name of data')
+    parser.add_argument('--location, -l', action = 'store', dest = 'location', default = '', required = False, help = 'Location of data')
 
     arguments = parser.parse_args()
     return {'name' : arguments.name,'location' : arguments.location}
@@ -28,4 +28,4 @@ else:
         print search_class.formatted_table({}, search_class.search_for_location(searching()['location']))
 
     else:
-        print search_class.formatted_table(search_class.search_for_name('*'), search_class.search_for_location('*'))
+        print search_class.formatted_table(search_class.search_for_name('*'), {})
