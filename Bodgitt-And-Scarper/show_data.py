fbf51cbac7c6fedad7464b83cfbd54d3fb0b2358
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2013-12-03 09:13:08 -0200

    Arrumada a chamada do parametro de linha de comando

--- Bodgitt-And-Scarper/show_data.py
+++ Bodgitt-And-Scarper/show_data.py
@@ -10,8 +10,8 @@ search_class = search.Search()
 def searching():
     parser = argparse.ArgumentParser(description = 'DESCRIPTION: Arquivo de busca para o dbSchema')
 
-    parser.add_argument('--name, -n', action = 'store', dest = 'name', default = '', required = False, help = 'Name of data')
-    parser.add_argument('--location, -l', action = 'store', dest = 'location', default = '', required = False, help = 'Location of data')
+    parser.add_argument('--name','-n', action = 'store', dest = 'name', default = '', required = False, help = 'Name of data')
+    parser.add_argument('--location','-l', action = 'store', dest = 'location', default = '', required = False, help = 'Location of data')
 
     arguments = parser.parse_args()
     return {'name' : arguments.name,'location' : arguments.location}
