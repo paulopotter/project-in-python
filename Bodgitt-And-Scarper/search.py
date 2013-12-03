7a631af33131f7e0f37df44a0574a2908ffccfbf
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2013-12-03 09:09:31 -0200

    Refatorada a funçao de exibir em forma de tabela e com isso foi criado uma funçao para e exibiçao de linhas

--- Bodgitt-And-Scarper/search.py
+++ Bodgitt-And-Scarper/search.py
@@ -30,20 +30,28 @@ class Search(object):
         table_header = table_column_widths.format("#", "Name", "Location", "Specialties", "Size", "Rate", "Owner")
         table_header += "\n+" + "="*204 + "+"
 
-        table_data = ""
-        number_of_line = 0
-
-        for values in dictionary_name:
-            number_of_line += 1
-            table_data += table_column_widths.format(number_of_line, values["name"], values["location"], values["specialties"], values["size"], values["rate"], values["owner"])
-            table_data += "\n+" + "-"*204 + "+\n"
-
-        for values in dictionary_location:
-            number_of_line += 1
-            table_data += table_column_widths.format(number_of_line, values["name"], values["location"], values["specialties"], values["size"], values["rate"], values["owner"])
-            table_data += "\n+" + "-"*204 + "+\n"
+        name = self.line_of_table(table_column_widths, dictionary_name)
+        location = self.line_of_table(table_column_widths, dictionary_location, len(dictionary_name))
 
         if len(dictionary_name) + len(dictionary_location) >= 1:
-            return table_header + '\n'+ table_data
+            return table_header + '\n'+ name + '\n'+ location
         else:
             return 'ERROR'
+
+    def line_of_table(self, table_column_widths, dictionary_for_lines, number_of_line = 0):
+        table_data = ""
+
+        for values in dictionary_for_lines:
+            number_of_line += 1
+            table_data += table_column_widths.format(
+                number_of_line,
+                values["name"],
+                values["location"],
+                values["specialties"],
+                values["size"],
+                values["rate"],
+                values["owner"]
+            )
+            table_data += "\n+" + "-"*204 + "+\n"
+
+        return table_data
