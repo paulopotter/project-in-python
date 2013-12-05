0fbb7155dd944ad70dba06f1b3fd05d494ab037a
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2013-12-05 10:49:05 -0200

    Refatorado o teste para o novo formato do seach_control

--- Bodgitt-And-Scarper/test_of_search_control.py
+++ Bodgitt-And-Scarper/test_of_search_control.py
@@ -1,12 +1,13 @@
 import unittest
-from search import Search
-from search import RecordNotFoundException
+import search_control
+from my_exceptions import RecordNotFoundException
+from formatting_of_data import FormattingOfData
 
 
 class TestSearch(unittest.TestCase):
 
     def setUp(self):
-        self.search_class = Search()
+        self.search_class = search_control.SearchControl()
 
     def test_name_of_search(self):
         name = self.search_class.search_for('B', 'name')
@@ -17,16 +18,13 @@ class TestSearch(unittest.TestCase):
         self.assertIn('Smallville', location[0].get('location'))
 
     def test_amount_of_search_all(self):
-        self.assertEqual(len(self.search_class.search_for('*', 'name')), 182)
-
-    def test_error_of_null_search(self):
-        self.assertIn('ERROR', self.search_class.formatted_table({'name': '', 'location': ''}))
+        self.assertEqual(len(self.search_class.search_for('*', 'name')), 29)
 
     def test_find_of_criteria(self):
         self.assertEqual(self.search_class.find('Buo'), [0, 5, 18, 25])
 
     def test_read_a_record(self):
-        self.assertEqual(self.search_class.read(1), self.formatting().formatted_records()[1])
+        self.assertEqual(self.search_class.read(1), FormattingOfData().formatted_records()[1])
 
     def test_read_exception(self):
         self.assertRaises(RecordNotFoundException, self.search_class.read, 30)
