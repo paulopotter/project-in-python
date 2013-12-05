cf1fe3966aa2a7bb287f84968e0da46c1c905c86
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2013-12-05 12:00:06 -0200

    adicionado a formataçao de tabela

--- Bodgitt-And-Scarper/show_data.py
+++ Bodgitt-And-Scarper/show_data.py
@@ -1,7 +1,8 @@
+# coding:utf-8
 import argparse
-import search
+import search_control
 
-search_class = search.Search()
+search_class = search_control.SearchControl()
 
 def searching():
     parser = argparse.ArgumentParser(description = 'DESCRIPTION: Arquivo de busca para o dbSchema')
@@ -25,9 +26,47 @@ def searching():
 
     return {'name' : arguments.name, 'location' : arguments.location}
 
+def formatted_table(dictionary_values):
+    table_column_widths = "| {0:2} | {1:32} | {2:64} | {3:64} | {4:6} | {5:8} | {6:8} |"
+    table_header = table_column_widths.format(
+        "ID", "Name", "Location",
+        "Specialties", "Size", "Rate", "Owner"
+    )
+    table_header += "\n+" + "=" * 204 + "+"
+
+    table_line = ''
+
+    for dict_key, dict_value in dictionary_values.items():
+        if dict_value != '':
+            table_line += line_of_table(
+                table_column_widths, search_class.search_for(dict_value, dict_key))
+
+    if table_line != '':
+        return table_header + '\n' + table_line
+    else:
+        return 'ERROR: Não existe registro com o(s) parametros passados'
+
+
+def line_of_table(table_column_widths, dictionary_for_lines):
+    table_data = ""
+
+    for values in dictionary_for_lines:
+        table_data += table_column_widths.format(
+            values['id'],
+            values["name"],
+            values["location"],
+            values["specialties"],
+            values["size"],
+            values["rate"],
+            values["owner"]
+        )
+        table_data += "\n+" + "-" * 204 + "+\n"
+
+    return table_data
+
 
 if searching()['name'] == '' and searching()['location'] == '':
-    print search_class.formatted_table({'name': '*'})
+    print formatted_table({'name': '*'})
 else:
-    print search_class.formatted_table(searching())
+    print formatted_table(searching())
 
