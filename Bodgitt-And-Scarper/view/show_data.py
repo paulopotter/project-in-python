594ac64e102029cb0ff936ec36ecd01ac2541f8d
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2013-12-05 08:22:46 -0200

    Arrumando os arquivos em pastas para o modelo MVC

---
+++ Bodgitt-And-Scarper/view/show_data.py
@@ -0,0 +1,33 @@
+import argparse
+import search
+
+search_class = search.Search()
+
+def searching():
+    parser = argparse.ArgumentParser(description = 'DESCRIPTION: Arquivo de busca para o dbSchema')
+
+    parser.add_argument('--name','-n',
+        action = 'store',
+        dest = 'name',
+        default = '',
+        required = False,
+        help = 'Name of data'
+    )
+    parser.add_argument('--location','-l',
+        action = 'store',
+        dest = 'location',
+        default = '',
+        required = False,
+        help = 'Location of data'
+    )
+
+    arguments = parser.parse_args()
+
+    return {'name' : arguments.name, 'location' : arguments.location}
+
+
+if searching()['name'] == '' and searching()['location'] == '':
+    print search_class.formatted_table({'name': '*'})
+else:
+    print search_class.formatted_table(searching())
+
