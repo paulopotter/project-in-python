8e19365943d845fc6870f9fad601bcf1b1c43fcb
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2013-12-05 10:11:38 -0200

    Alterado as chamadas dbSchema para data_conn

--- Bodgitt-And-Scarper/formatting_of_data.py
+++ Bodgitt-And-Scarper/formatting_of_data.py
@@ -1,21 +1,21 @@
 # coding:utf-8
-import dbSchema
+import data_conn
 
 
 class FormattingOfData:
 
     def __init__(self):
-        self.db_schema = dbSchema.DbSchema()
+        self.data_conn = data_conn.DataConn()
 
     def formatted_records(self):
-        record  = self.db_schema.records()
+        record  = self.data_conn.records()
         formatted_record = []
 
-        for number_of_records in range(self.db_schema.number_of_records()):
+        for number_of_records in range(self.data_conn.number_of_records()):
             dicionario = {'id': number_of_records}
 
-            for number_of_fields in range(self.db_schema.start_of_file()["number_of_fields"]):
-                dicionario[self.db_schema.meta_dada[number_of_fields]['field_name']] = record[number_of_records][number_of_fields]
+            for number_of_fields in range(self.data_conn.start_of_file()["number_of_fields"]):
+                dicionario[self.data_conn.meta_dada[number_of_fields]['field_name']] = record[number_of_records][number_of_fields]
 
             dicionario["byte_flag"] =  record[number_of_records][6]
             formatted_record.append(dicionario)
