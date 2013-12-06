796b9e6801821607bf77f0758f63b0682b14e88f
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2013-12-06 17:28:32 -0200

    Adicionado os parametros find e read. O arquivo exibe os dados formatados de acordo com o parse passado. Retirado funçao de formataçao de tabela do arquivo

--- Bodgitt-And-Scarper/show_data.py
+++ Bodgitt-And-Scarper/show_data.py
@@ -1,72 +1,63 @@
 # coding:utf-8
 import argparse
 import search_control
+import formatting_of_table
 
 search_class = search_control.SearchControl()
+formatting_table_class = formatting_of_table.FormattingData()
 
 def searching():
-    parser = argparse.ArgumentParser(description = 'DESCRIPTION: Arquivo de busca para o dbSchema')
+    parser = argparse.ArgumentParser(description = 'Arquivo de busca para o dbSchema')
 
-    parser.add_argument('--name','-n',
+    parser.add_argument('--name',
         action = 'store',
         dest = 'name',
         default = '',
         required = False,
         help = 'Name of data'
     )
-    parser.add_argument('--location','-l',
+    parser.add_argument('--location',
         action = 'store',
         dest = 'location',
         default = '',
         required = False,
         help = 'Location of data'
     )
+    parser.add_argument('--read',
+        action = 'store',
+        dest = 'read',
+        default = '',
+        required = False,
+        help = 'Read data'
+    )
+    parser.add_argument('--find',
+        action = 'store',
+        dest = 'find',
+        default = '',
+        required = False,
+        help = 'Find data and return Id array'
+    )
 
     arguments = parser.parse_args()
 
-    return {'name' : arguments.name, 'location' : arguments.location}
-
-def formatted_table(dictionary_values):
-    table_column_widths = "| {0:2} | {1:32} | {2:64} | {3:64} | {4:6} | {5:8} | {6:8} |"
-    table_header = table_column_widths.format(
-        "ID", "Name", "Location",
-        "Specialties", "Size", "Rate", "Owner"
-    )
-    table_header += "\n+" + "=" * 204 + "+"
-
-    table_line = ''
-
-    for dict_key, dict_value in dictionary_values.items():
-        if dict_value != '':
-            table_line += line_of_table(
-                table_column_widths, search_class.search_for(dict_value, dict_key))
-
-    if table_line != '':
-        return table_header + '\n' + table_line
+    if arguments.read != '':
+        return {'read': arguments.read}
+    elif arguments.find != '':
+        return {'find': arguments.find}
     else:
-        return 'ERROR: Não existe registro com o(s) parametros passados'
+        if arguments.name == '' and arguments.location == '':
+            return {'name': '*', 'location': arguments.location}
+        else:
+            return {'name': arguments.name, 'location': arguments.location}
 
+search = searching()
 
-def line_of_table(table_column_widths, dictionary_for_lines):
-    table_data = ""
+if search.has_key('read'):
+    read = search_class.read(int(search['read']))
+    print formatting_table_class.formatted_table({'id': read['id']})
 
-    for values in dictionary_for_lines:
-        table_data += table_column_widths.format(
-            values['id'],
-            values["name"],
-            values["location"],
-            values["specialties"],
-            values["size"],
-            values["rate"],
-            values["owner"]
-        )
-        table_data += "\n+" + "-" * 204 + "+\n"
+elif search.has_key('find'):
+    print search_class.find(search['find'])
 
-    return table_data
-
-
-if searching()['name'] == '' and searching()['location'] == '':
-    print formatted_table({'name': '*'})
 else:
-    print formatted_table(searching())
-
+    print formatting_table_class.formatted_table(searching())
