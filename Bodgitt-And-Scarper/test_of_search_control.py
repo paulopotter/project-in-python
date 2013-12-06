5e57be8662c91217f5dc4288689a90dc1a802951
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2013-12-06 17:31:08 -0200

    Retirado o vinculo de outros arquivos do teste, agora o teste só usa o arquivo a ser testado.

--- Bodgitt-And-Scarper/test_of_search_control.py
+++ Bodgitt-And-Scarper/test_of_search_control.py
@@ -1,7 +1,6 @@
 import unittest
 import search_control
 from my_exceptions import RecordNotFoundException
-from formatting_of_data import FormattingOfData
 
 
 class TestSearch(unittest.TestCase):
@@ -9,13 +8,17 @@ class TestSearch(unittest.TestCase):
     def setUp(self):
         self.search_class = search_control.SearchControl()
 
-    def test_name_of_search(self):
+    def test_name_of_search_with_upper_letter(self):
         name = self.search_class.search_for('B', 'name')
         self.assertIn('Buon', name[0].get('name'))
 
-    def test_location_of_search(self):
-        location = self.search_class.search_for('S', 'location')
+    def test_location_of_search_with_lower_letter(self):
+        location = self.search_class.search_for('s', 'location')
         self.assertIn('Smallville', location[0].get('location'))
+
+    def test_search_for_id_number(self):
+        id_number = self.search_class.search_for(0, 'id')
+        self.assertEqual(0, id_number[0]['id'])
 
     def test_amount_of_search_all(self):
         self.assertEqual(len(self.search_class.search_for('*', 'name')), 29)
@@ -24,7 +27,16 @@ class TestSearch(unittest.TestCase):
         self.assertEqual(self.search_class.find('Buo'), [0, 5, 18, 25])
 
     def test_read_a_record(self):
-        self.assertEqual(self.search_class.read(1), FormattingOfData().formatted_records()[1])
+        dicto = {'byte_flag': 0,
+                 'name': 'Swanders & Flaughn              ',
+                 'rate': '$55.00  ',
+                 'specialties': 'Painting, Air Conditioning                                      ',
+                 'location': 'Smallville                                                      ',
+                 'owner': '        ',
+                 'id': 1,
+                 'size': '7     '
+                 }
+        self.assertEqual(self.search_class.read(1), dicto)
 
     def test_read_exception(self):
         self.assertRaises(RecordNotFoundException, self.search_class.read, 30)
