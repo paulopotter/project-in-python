7dfe4617efa1727945883f9fca4d48a5d27da6df
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2013-12-11 11:39:59 -0200

    Arrumado o metodo find para o modelo pedido no exercicio. Agora ele recebe um dicionario e retorna uma lista de inteiros (id) dos dados que correspondem

--- Bodgitt-And-Scarper/search_control.py
+++ Bodgitt-And-Scarper/search_control.py
@@ -7,30 +7,33 @@ from my_exceptions import RecordNotFoundException
 class SearchControl(object):
 
     def search_for(self, value, field_to_search):
+
+        field_to_search = field_to_search.lower()
         db_schema = data_conn.DataConn()
         formatted_of_data = formatting_of_data.FormattingOfData()
-        match_values = []
         formatted_records = formatted_of_data.formatted_records()
+        match_values = []
 
         for number_of_lines in range(db_schema.number_of_records()):
             formatted_record = formatted_records[number_of_lines][field_to_search]
 
-            if value == '*' or
-            (type(value) == str and value.lower() == formatted_record.lower()[:len(value)]) or
-            (type(value) == int and value == formatted_record):
+            if value == '*' or (type(value) == str and value.lower() == formatted_record.lower()[:len(value)] and value != '') or (type(value) == int and value == formatted_record):
                     match_values.append(formatted_records[number_of_lines])
 
         return match_values
 
     def find(self, criteria):
-        find_value = self.search_for(criteria, 'name')
-        find_value += self.search_for(criteria, 'location')
+        find_value = []
         positions = []
+        for x in range(len(criteria)):
+            find_value += self.search_for(criteria[criteria.keys()[x]], criteria.keys()[x])
+
         for line in range(len(find_value)):
             positions.append(find_value[line]['id'])
 
         new_position = list(set(positions))
         new_position.sort()
+
         return new_position
 
     def read(self, recNo):
