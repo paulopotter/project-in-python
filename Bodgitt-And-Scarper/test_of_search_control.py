387d5e22825657c76995096f5d5e2cb3d144a9a1
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2013-12-13 10:13:41 -0200

    Refeito todos os testes para o novo formato do search_control

--- Bodgitt-And-Scarper/test_of_search_control.py
+++ Bodgitt-And-Scarper/test_of_search_control.py
@@ -1,49 +1,33 @@
 import unittest
 import search_control
-from my_exceptions import RecordNotFoundException
+# from my_exceptions import RecordNotFoundException
 
 
 class TestSearch(unittest.TestCase):
 
     def setUp(self):
-        self.search_class = search_control.SearchControl()
+        self.search = search_control.SearchControl()
 
-    def test_name_of_search_with_upper_letter(self):
-        name = self.search_class.search_for('B', 'name')
-        self.assertIn('Buon', name[0].get('name'))
+    def test_find_return_type(self):
+        self.assertEqual(type(self.search.find(**{'name': None, 'location': 'Smallville'})), list)
 
-    def test_location_of_search_with_lower_letter(self):
-        location = self.search_class.search_for('s', 'location')
-        self.assertIn('Smallville', location[0].get('location'))
+    def test_find_location(self):
+        self.assertEqual(self.search.find(**{'name': None, 'location': 'X'}), [19, 20, 21])
 
-    def test_search_for_id_number(self):
-        id_number = self.search_class.search_for(0, 'id')
-        self.assertEqual(0, id_number[0]['id'])
+    def test_find_name(self):
+        self.assertEqual(self.search.find(**{'name': 'Buon', 'location': None}), [0, 5, 18, 25])
 
-    def test_amount_of_search_all(self):
-        self.assertEqual(len(self.search_class.search_for('*', 'name')), 29)
+    def test_find_all(self):
+        self.assertEqual(self.search.find(**{'name': None, 'location': None}), range(29))
 
-    def test_find_of_criteria(self):
-        self.assertEqual(self.search_class.find({'Name': 'Buo'}), [0, 5, 18, 25])
+    def test_read_return_type(self):
+        self.assertEqual(type(self.search.read(0)), list)
 
-    def test_read_a_record(self):
-        dicto = {'byte_flag': 0,
-                 'name': 'Swanders & Flaughn              ',
-                 'rate': '$55.00  ',
-                 'specialties': 'Painting, Air Conditioning                                      ',
-                 'location': 'Smallville                                                      ',
-                 'owner': '        ',
-                 'id': 1,
-                 'size': '7     '
-                 }
-        self.assertEqual(self.search_class.read(1), dicto)
+    def test_read(self):
+        self.assertEqual(self.search.read(0), ['Buonarotti & Company            ', 'Smallville                                                      ', 'Air Conditioning, Painting, Painting                            ', '10    ', '$40.00  ', '        ', 0])
 
-    def test_read_exception(self):
-        self.assertRaises(RecordNotFoundException, self.search_class.read, 30)
-
-    def test_duplicate_data_in_find_method(self):
-        self.assertEqual(self.search_class.find({'name': 's', 'location': 's'}), [0, 1, 12, 22])
-
+    def test_read_error(self):
+        self.assertEqual(self.search.read(30), 'Record with this position not found.')
 
 if __name__ == '__main__':
     unittest.main()
