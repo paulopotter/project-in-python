82d2adedf09692fb3d22b80098919cc7cb72d7b0
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2013-12-13 10:24:18 -0200

    Refeito todo o search control de uma forma mais simples. Agora o find() retorna uma lista de id e o read() retorna a lista do dados da linha expecificada

--- Bodgitt-And-Scarper/search_control.py
+++ Bodgitt-And-Scarper/search_control.py
@@ -1,43 +1,39 @@
 # coding:utf-8
-import data_conn
-import formatting_of_data
+from data_conn import DataConn
 from my_exceptions import RecordNotFoundException
 
 
 class SearchControl(object):
 
-    def search_for(self, value, field_to_search):
-
-        field_to_search = field_to_search.lower()
-        db_schema = data_conn.DataConn()
-        formatted_of_data = formatting_of_data.FormattingOfData()
-        formatted_records = formatted_of_data.formatted_records()
-        match_values = []
-
-        for number_of_lines in range(db_schema.number_of_records()):
-            formatted_record = formatted_records[number_of_lines][field_to_search]
-
-            if value == '*' or (type(value) == str and value.lower() == formatted_record.lower()[:len(value)] and value != '') or (type(value) == int and value == formatted_record):
-                    match_values.append(formatted_records[number_of_lines])
-
-        return match_values
-
-    def find(self, criteria):
-        find_value = []
+    def find(self, **criteria):
+        records = DataConn().records()
         positions = []
-        for x in range(len(criteria)):
-            find_value += self.search_for(criteria[criteria.keys()[x]], criteria.keys()[x])
 
-        for line in range(len(find_value)):
-            positions.append(find_value[line]['id'])
+        if criteria['name'] is None and criteria['location'] is None:
+            for line in range(len(records)):
+                positions.append(line)
 
-        new_position = list(set(positions))
-        new_position.sort()
+        if criteria['name'] is not None:
+            for line in range(len(records)):
+                if records[line][0].lower().find(criteria['name'].lower()) != -1 and records[line][0].lower().find(criteria['name'].lower()) == 0:
 
-        return new_position
+                    positions.append(line)
+
+        if criteria['location'] is not None:
+            for line in range(len(records)):
+                if records[line][1].lower().find(criteria['location'].lower()) != -1 and records[line][1].lower().find(criteria['location'].lower()) == 0:
+
+                    positions.append(line)
+
+        line_records = list(set(positions))
+        line_records.sort()
+
+        return line_records
 
     def read(self, recNo):
         try:
-            return self.search_for(recNo, 'id')[0]
+            records = DataConn().records()
+            line_value = records[recNo]
+            return line_value
         except Exception:
             raise RecordNotFoundException
