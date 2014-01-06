491970759a68715d09792d2cc87fae047019cd70
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2014-01-06 19:32:52 -0200

    Adicionada a classe DuplicateKeyException

--- Bodgitt-And-Scarper/my_exceptions.py
+++ Bodgitt-And-Scarper/my_exceptions.py
@@ -3,3 +3,8 @@
 class RecordNotFoundException(Exception):
     def __str__(self):
         return 'Record with this position not found.'
+
+
+class DuplicateKeyException(Exception):
+    def __str__(self):
+        return 'Record with this position is duplicate.'
