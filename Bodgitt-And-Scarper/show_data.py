1eb26a5c63824b57dc8e7ae8e373a8182b02c45b
Paulo Vitor Oliveira <paulofrauches@gmail.com>
2014-01-09 14:37:05 -0200

    Adicionado o argpase do create para receber um lista de valores

--- Bodgitt-And-Scarper/show_data.py
+++ Bodgitt-And-Scarper/show_data.py
@@ -11,7 +11,7 @@ class CommandTerminal(object):
         parser.add_argument('-n', '--name', action='store', dest='name', default=None, required=False, help='Search and list all names according to specified criteria.')
         parser.add_argument('-l', '--location', action='store', dest='location', default=None, required=False, help='Search and list all locations according to specified criteria.')
         parser.add_argument('-a', '--and', action='store_true', dest='search_and', default=False, required=False, help='Se utilizado será feito a busca no nome E location( name e location obrigatorios).')
-        parser.add_argument('-c', '--create', action='store', dest='create', default=None, required=False, help='Cria um novo registro.')
+        parser.add_argument('-c', '--create', action='store', nargs='+', dest='create', default=None, required=False, help='Cria um novo registro.')
         parser.add_argument('-d', '--delete', action='store', dest='delete', default=None, required=False, help='Apaga um registro.')
 
         arguments = parser.parse_args()
@@ -19,33 +19,38 @@ class CommandTerminal(object):
         self.name = arguments.name
         self.location = arguments.location
         self.search_and = arguments.search_and
+        self.create = arguments.create
+        self.delete = arguments.delete
 
         self.crud = crud.CRUD()
         self.find = self.crud.find(**{'name': self.name, 'location': self.location, 'search_and': self.search_and})
 
-        if self.find == []:
-            print 'Sorry, we could not find the value of the search.'
-
+        if self.create:
+            print crud.CRUD().create(*self.create)
         else:
-            text_table = texttable.Texttable()
-            header = ['#', 'Name', 'Location', 'Specialties', 'Size', 'Rate', 'Owner']
+            if self.find == []:
+                print 'Sorry, we could not find the value of the search.'
 
-            text_table.header(header)
-            bla = 0
+            else:
+                text_table = texttable.Texttable()
+                header = ['#', 'Name', 'Location', 'Specialties', 'Size', 'Rate', 'Owner']
 
-            for line_records in range(len(self.find)):
-                row = self.crud.read(self.find[line_records])
-                bla = bla + row[0]
+                text_table.header(header)
+                bla = 0
 
-                if row[-1] != 1:  # Se o byte flag for false, exibe
-                    row.pop(-1)  # Remove a exibiçao do byte flag
-                    text_table.add_row(row)
-                else:
-                    row.pop(-1)  # Remove a exibiçao do byte flag
+                for line_records in range(len(self.find)):
+                    row = self.crud.read(self.find[line_records])
+                    bla = bla + row[0]
 
-            text_table.set_cols_width([2, 25, 15, 50, 5, 10, 5])
-            text_table.set_cols_align(['c', 'l', 'l', 'l', 'c', 'l', 'l'])
-            print text_table.draw()
+                    if row[-1] != 1:  # Se o byte flag for false, exibe
+                        row.pop(-1)  # Remove a exibiçao do byte flag
+                        text_table.add_row(row)
+                    else:
+                        row.pop(-1)  # Remove a exibiçao do byte flag
+
+                text_table.set_cols_width([2, 25, 15, 50, 5, 10, 5])
+                text_table.set_cols_align(['c', 'l', 'l', 'l', 'c', 'l', 'l'])
+                print text_table.draw()
 
 
 if __name__ == '__main__':
