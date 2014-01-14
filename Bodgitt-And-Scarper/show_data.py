d4c70ea2efcebd4f2c6650ec2dd98ea2da4a0e41
Paulo Oliveira <paulofrauches@gmail.com>
2014-01-14 15:30:57 -0200

    Criado uma funçao para retorno da tabela (formata o texto para exibiçao em tabela). Arrumado o parse "delete" para store_true e o novo formato de exibiçao do delete

--- Bodgitt-And-Scarper/show_data.py
+++ Bodgitt-And-Scarper/show_data.py
@@ -12,7 +12,7 @@ class CommandTerminal(object):
         parser.add_argument('-l', '--location', action='store', dest='location', default=None, required=False, help='Procura e lista todos os registros com a \'location\' informada.')
         parser.add_argument('-a', '--and', action='store_true', dest='search_and', default=False, required=False, help='Se utilizado será feito a busca no \'name\' E \'location\'( \'name\' e \'location\' obrigatórios).')
         parser.add_argument('-c', '--create', action='store', nargs='+', dest='create', default=None, required=False, help='Cria um novo registro. Espaços serão considerados novos campos (para digitar um texto com espaço use aspas). \'name\' e \'location\' obrigatórios.')
-        parser.add_argument('-d', '--delete', action='store', dest='delete', nargs='+', default=None, required=False, help='Apaga registro com o ID passado (use espaços para apagar mais de um registro ao mesmo tempo). Se passado um texto, apaga todas as ocorrências deste texto.')
+        parser.add_argument('-d', '--delete', action='store_true', dest='delete', default=False, required=False, help='Apaga registro com o ID passado (use espaços para apagar mais de um registro ao mesmo tempo). Se passado um texto, apaga todas as ocorrências deste texto.')
 
         parser.add_argument('-u', '--update', action='store', dest='update', nargs='*', default=None, required=False, help='Adiciona o Id do usuario no registro informado, o primeiro valor é o campo a ser editado e o segundo o ID do usuario.(Ex.: 00 11)')
 
@@ -35,49 +35,58 @@ class CommandTerminal(object):
                 print verify_entry_type
 
         elif arguments.delete:
-            try:
-                int(arguments.delete[0])
-                for recNo in arguments.delete:
-                    self.crud.delete(recNo)
-                    print 'Registro [%s] apagado com sucesso!' % recNo
+            if not arguments.name or not arguments.location:
+                print 'ERRO: \'name\' e/ou \'location\' necessário'
+            else:
+                print self.draw_table(self.find)
 
-            except:
-                value = ' '.join(str(x) for x in arguments.delete)
-                records = self.crud.find({'name': str(value), 'location': None, 'search_and': False})
-                if records:
-                    for recNo in records:
-                        self.crud.delete(int(recNo))
-                        print 'Registro [%s] apagado com sucesso!' % recNo
-                else:
-                    print 'ERRO: Registro não encontrado'
+                if self.find:
+                    delete_yes_not = raw_input('\nDeseja deletar todos os registros exibidos? \nDigite SIM para apagar e NAO para cancelar a exclusão. ')
+
+                    if delete_yes_not.lower() == 'sim' or delete_yes_not.lower() == 's':
+                            excluded_records = 0
+                            for recNo in self.find:
+                                self.crud.delete(recNo)
+                                excluded_records += 1
+
+                            print '%i registro(s) apagado(s) com sucesso!' % excluded_records
+
+                    elif delete_yes_not.lower() == 'nao' or delete_yes_not.lower() == 'não' or delete_yes_not.lower() == 'n':
+                        print 'A exclusão foi cancelada'
+                    else:
+                        print 'Opcão inválida, a exclusão foi cancelada.'
 
         elif arguments.update:
             if len(arguments.update) != 2:
                 print 'ERRO: São necessario 2 valores, você digitou %i. \nLembrando: o primeiro valor é o NUMERO do registro a ser editado e o segundo o ID do usuario' % len(arguments.update)
             else:
                 self.crud.update(arguments.update[0], {'owner': arguments.update[1]})
-                print 'Registro [%s] atualizado com sucesso!' % arguments.update[0]
+                print 'Registro atualizado com sucesso!'
 
         else:
-            if self.find == []:
-                print 'Registro informado não encontrado.'
+            print self.draw_table(self.find)
 
-            else:
-                text_table = texttable.Texttable()
-                header = ['#', 'Name', 'Location', 'Specialties', 'Size', 'Rate', 'Owner']
+    def draw_table(self, find):
+        if find == []:
+            return 'Registro informado não encontrado.'
 
-                text_table.header(header)
+        else:
+            text_table = texttable.Texttable()
+            header = ['#', 'Name', 'Location', 'Specialties', 'Size', 'Rate', 'Owner']
 
-                for line_records in range(len(self.find)):
-                    row = self.crud.read(self.find[line_records])
+            text_table.header(header)
+            x = 1
+            for line_records in range(len(find)):
+                row = self.crud.read(find[line_records])
 
-                    if row[-1] != 1:  # Se o byte flag for false, exibe
-                        row.pop(-1)  # Remove a exibiçao do byte flag
-                        text_table.add_row(row)
+                if row[-1] != 1:  # Se o byte flag for false, exibe
+                    row.pop(-1)  # Remove a exibiçao do byte flag
+                    text_table.add_row([x] + row)
+                    x += 1
 
-                text_table.set_cols_width([2, 32, 32, 50, 6, 8, 8])
-                text_table.set_cols_align(['c', 'l', 'l', 'l', 'c', 'l', 'l'])
-                print text_table.draw()
+            text_table.set_cols_width([2, 32, 32, 50, 6, 8, 8])
+            text_table.set_cols_align(['c', 'l', 'l', 'l', 'c', 'l', 'l'])
+            return text_table.draw()
 
 
 if __name__ == '__main__':
