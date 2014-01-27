3dc54a5417e663980ac821522ce9ac1515c0df4d
Paulo Oliveira <paulofrauches@gmail.com>
2014-01-27 16:08:30 -0200

    Arquivos movidos ou renomeados

--- Bodgitt-And-Scarper/app/static/script.js
+++
@@ -1,6 +0,0 @@
-function verify_search(){
-  if(document.getElementById('input-name').value.length < 1 && document.getElementById('input-location').value.length < 1){
-      document.getElementById('search-msg').innerHTML = "Campo < name > ou < location > necessário!";
-      return false;
-  }
-}
