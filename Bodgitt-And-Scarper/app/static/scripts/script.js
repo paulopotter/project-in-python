4f2dab4201566e6efd08afdeb727b8dfdf6eceae
Paulo Oliveira <paulofrauches@gmail.com>
2014-01-29 17:42:30 -0200

    Criado a funçao para a mascara do formulario. Alterado, na funcao close_msg, a forma de atualizar a pagina.

--- Bodgitt-And-Scarper/app/static/scripts/script.js
+++ Bodgitt-And-Scarper/app/static/scripts/script.js
@@ -13,11 +13,16 @@ function searchResult(str){
   ajax_require('table-body',"/search?q=" + str);
 }
 
-
 function addRecords(){
   var div_msg = document.getElementById('background-msg');
   div_msg.style.display = 'block';
   ajax_require('background-msg', "/create");
+}
+
+function verify_new_record(meta_data){
+  var div_msg = document.getElementById('background-msg');
+  div_msg.style.display = 'block';
+  ajax_require('body', "/create-validation");
 }
 
 function remove_record(name, location, validate){
@@ -25,12 +30,11 @@ function remove_record(name, location, validate){
   if (validate) {
     ajax_require('background-msg', '/remove-validation?name=' + name + '&location=' + location)
   }else{
-  var div_msg = document.getElementById('background-msg');
-  div_msg.style.display = 'block';
+    var div_msg = document.getElementById('background-msg');
+    div_msg.style.display = 'block';
     ajax_require('background-msg', '/remove?name=' + name + '&location=' + location);
   }
 }
-
 
 function edit_record(name, location){
   var div_msg = document.getElementById('background-msg');
@@ -38,10 +42,24 @@ function edit_record(name, location){
   ajax_require('background-msg', "/edit?name=" + name + '&location=' + location);
 }
 
-function close_msg(redirect_home){
+function close_msg(reload_page){
   var div_msg = document.getElementById('background-msg');
   div_msg.style.display = 'none';
-  if (redirect_home) {
-    ajax_require('body', '/')
+  if (reload_page) {
+    window.location.reload()
+  }
+}
+
+function mask(evt, field){
+  switch(field){
+    case 'size':
+    case 'owner':
+      var charCode = (evt.which) ? evt.which : event.keyCode
+      if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+      return true;
+      break;
+    case 'rate':
+    break;
   }
 }
